d7f866ec21ce697f9483d04670d2cacc6705da77
drinker <drink-182@proton.me>
2026-04-19 00:06:12 -0600

    trying to fix gitignore

--- stage_one/target/debug/deps/stage_one-77c02de00606df58.d
+++
@@ -1,5 +0,0 @@
-/home/d/Projects/bootloader/stage_one/target/debug/deps/stage_one-77c02de00606df58.d: src/main.rs
-
-/home/d/Projects/bootloader/stage_one/target/debug/deps/stage_one-77c02de00606df58: src/main.rs
-
-src/main.rs:
--- stage_one/target/debug/stage_one.d
+++
@@ -1,1 +0,0 @@
-/home/d/Projects/bootloader/stage_one/target/debug/stage_one: /home/d/Projects/bootloader/stage_one/src/main.rs
