2ac2e290140fb2ae7b69dd03d06af97b815ad83b
gummybear69420 <gummybear69420>
2026-07-23 11:14:54 -0600

    updated

--- stage_one_16_bit/main.c
+++ stage_one_16_bit/main.c
@@ -11,26 +11,52 @@ _hacked_function _asm_entry(){
 		.code16
 
 		/*
-		 * clears the code segment register, sometimes full of garbage
+		 * The only way to set the CS register is to perform a long jump. Qemu automatically ensures that the code segment is 0, however this is not garunteed on hardware.
+		 * 
+		 * As far as I know, the reason this is done is to ensure that there will not be undefined behaviour if the segment register is set to 0x7c00 and the instruction pointer is set to 0. The
+		 * jump to the memory address 0:_clear_cs_reg ensures that cs = 0 and the instruction pointer is pointing to the correct memory address. 
+		 *
+		 * *Needs fact checking. 
 		 * */
 		ljmp $0, $_clear_cs_reg
 
 		_clear_cs_reg: 
 		
 		/*
-		 * Clearing all the registers 
+		 * Clearing all of the segment registers. Simplifies memory addressing with segment:offset scheme if the segment register is set to 0. Xoring AX with itself sets the value of 
+		 * the register to 0. 
 		 * */
 		xor %ax, %ax
-		mov %
-			
+		mov %ax, %ds
+		mov %ax, %es
+		mov %ax, %ss
+		mov %ax, %fs
+		mov %ax, %gs
+
+		/*
+		 * TODO: enable A20. 
+		 * Qemu automatically enables the A20 line, so I wont bother with that headace right now. 
+		 * */
+
 		/* Sanity check */	
 		movb $0x0E, %ah 
 		movb $'h', %al 
 		int $0x10
 
+		/*
+		 * Disable interupts
+		 * */
+		cli
+			
+		
+
 	        call _c_entry
 	)");	
 }
+
+typedef struct __attribute__((packed)) {
+	
+} global_descriptor_table_r;  
 
 void _c_entry(){
 	while(1); 
